33bf5f5bf5af48788f62d72c5ff62bc305011260
sudipthon <65460694+sudipthon@users.noreply.github.com>
2023-09-09 03:51:00 +0545

    Update settings.py

--- StudyBud/settings.py
+++ StudyBud/settings.py
@@ -194,7 +194,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sudip00kc@gmail.com'
-EMAIL_HOST_PASSWORD = 'pjtbicdekobqontn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_FROM_EMAIL = 'noreply<no_reply@domain.com>'
